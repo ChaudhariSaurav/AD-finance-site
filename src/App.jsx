5b930da478f5cfc1caa1ba697554fb5a4787d6c7
Saurav kumar chaudhary <98453545+ChaudhariSaurav@users.noreply.github.com>
2024-07-11 08:25:52 +0000

    update home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ValidAuthroutes } from './common/validRoutes';
-import Navbar from './components/Navbar';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ValidAuthroutes } from "./common/validRoutes";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <div className=" bg-center bg-cover">
-      <Routes>
-        {ValidAuthroutes.map((route, index) => (
-          <Route key={index} path={route.path} element={route.element} />
-        ))}
-      </Routes>
+        <Routes>
+          {ValidAuthroutes.map((route, index) => (
+            <Route key={index} path={route.path} element={route.element} />
+          ))}
+        </Routes>
       </div>
     </Router>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { Menu } from '@headlessui/react';
-import { MenuIcon, XIcon } from '@heroicons/react/outline';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Menu } from "@headlessui/react";
+import { MenuIcon, XIcon } from "@heroicons/react/outline";
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -9,12 +9,29 @@ function Navbar() {
   return (
     <nav className="bg-blue-500">
       <div className="container mx-auto px-4 py-3 flex justify-between items-center">
-        <div className="text-white text-xl font-bold">AD Finance</div>
+        <div className="text-white text-xl font-bold">
+          <Link to="/">
+            <img
+              src="https://ik.imagekit.io/laxmifinance/IMG-20240603-WA0018.jpg?updatedAt=1720653923997"
+              alt="Jr Groups"
+              width={70}
+              height={70}
+            />
+          </Link>
+        </div>
         <div className="hidden md:flex space-x-4">
-          <Link to="/" className="text-white hover:text-gray-300">Home</Link>
-          <Link to="/about" className="text-white hover:text-gray-300">About Us</Link>
-          <Link to="/contact" className="text-white hover:text-gray-300">Contact Us</Link>
-          <Link to="/Gallary" className="text-white hover:text-gray-300">Gallary</Link>
+          <Link to="/" className="text-white hover:text-gray-300">
+            Home
+          </Link>
+          <Link to="/about" className="text-white hover:text-gray-300">
+            About Us
+          </Link>
+          <Link to="/contact" className="text-white hover:text-gray-300">
+            Contact Us
+          </Link>
+          <Link to="/Gallary" className="text-white hover:text-gray-300">
+            Gallary
+          </Link>
         </div>
         <div className="md:hidden">
           <button onClick={() => setIsOpen(!isOpen)}>
@@ -33,7 +50,7 @@ function Navbar() {
               {({ active }) => (
                 <Link
                   to="/"
-                  className={`block px-4 py-2 text-white ${active ? 'bg-blue-700' : ''}`}
+                  className={`block px-4 py-2 text-white ${active ? "bg-blue-700" : ""}`}
                   onClick={() => setIsOpen(false)}
                 >
                   Home
@@ -44,7 +61,7 @@ function Navbar() {
               {({ active }) => (
                 <Link
                   to="/about"
-                  className={`block px-4 py-2 text-white ${active ? 'bg-blue-700' : ''}`}
+                  className={`block px-4 py-2 text-white ${active ? "bg-blue-700" : ""}`}
                   onClick={() => setIsOpen(false)}
                 >
                   About Us
@@ -55,7 +72,7 @@ function Navbar() {
               {({ active }) => (
                 <Link
                   to="/contact"
-                  className={`block px-4 py-2 text-white ${active ? 'bg-blue-700' : ''}`}
+                  className={`block px-4 py-2 text-white ${active ? "bg-blue-700" : ""}`}
                   onClick={() => setIsOpen(false)}
                 >
                   Contact Us
@@ -66,7 +83,7 @@ function Navbar() {
               {({ active }) => (
                 <Link
                   to="/Gallary"
-                  className={`block px-4 py-2 text-white ${active ? 'bg-blue-700' : ''}`}
+                  className={`block px-4 py-2 text-white ${active ? "bg-blue-700" : ""}`}
                   onClick={() => setIsOpen(false)}
                 >
                   Gallary
